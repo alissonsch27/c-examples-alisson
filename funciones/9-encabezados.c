5fd1cfbfebcd4a0dee6882dadd96a0f48a05a69a
alissonsch27 <alisson.sirias@ucr.ac.cr>
2024-04-28 15:12:39 -0600

    Cambios en main, agregando funcion4

--- funciones/9-encabezados.c
+++ funciones/9-encabezados.c
@@ -13,6 +13,14 @@ int main() {
     // Imprime el valor que retornó funcion3()
     printf("v = %d\n", v);
 
+
+    // Llama a funcion4
+    v = funcion4(10);
+
+    // Imprime el valor que retornó funcion4()
+    printf("v = %d\n", v);
+
+
     return 0;
 
 }
@@ -33,5 +41,13 @@ void funcion2(int a, float b) {
 int funcion3(int x) {
     printf("En funcion3()...\n");
     int y = x + 2;
+    return y;
+}
+
+// funcion4(): Imprime un mensaje. Retorna el valor que recibió como argumento
+// decrementado en 2
+int funcion3(int x) {
+    printf("En funcion4()...\n");
+    int y = x - 2;
     return y;
 }
